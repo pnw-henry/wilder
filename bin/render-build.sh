50e0bc49da72526e2d4a9cd25837c0342e0b98a2
Henry Escobar <henrye@gmail.com>
2024-04-24 15:53:51 -0700

    again..

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 # clean
 rm -rf public
 # build
@@ -11,3 +13,5 @@ npm install --prefix client && npm run build --prefix client
 bundle exec rake db:migrate
 # postbuild
 cp -a client/build/. public/
+#seed
+bundle exec rake db:seed
