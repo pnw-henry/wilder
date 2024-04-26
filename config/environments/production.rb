4368a01fd65b9eb16af6154c62e6c3918b48aeec
Henry Escobar <henrye@gmail.com>
2024-04-26 13:54:54 -0700

    oops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.assets.compile = true
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
